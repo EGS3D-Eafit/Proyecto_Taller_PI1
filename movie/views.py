9640fe800ab195af3ab348e716c7b659e35bf0f7
EGS3D-Eafit <159959074+EGS3D-Eafit@users.noreply.github.com>
2025-07-31 17:53:18 -0500

    nuevo archivo about.html

--- movie/views.py
+++ movie/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'home.html', {'name': 'David Ballesteros Jaimes'})
 
+def about(request):
+    return render(request, 'about.html', {'name': 'waos'})
 # Create your views here.
