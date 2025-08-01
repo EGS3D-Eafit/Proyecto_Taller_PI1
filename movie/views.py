d773dc297caa3f8f6cbce07e8bd431811b125b74
EGS3D-Eafit <159959074+EGS3D-Eafit@users.noreply.github.com>
2025-07-31 23:22:48 -0500

    visualizacion de peliculas con estilo

--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return render(request, 'home.html', {'name': 'David Ballesteros Jaimes'})
+from .models import Movie
 
+def home(request):
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
+
+def about(request):
+    return render(request, 'about.html', {'name': 'waos'})
 # Create your views here.
--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -12,8 +12,15 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
+from django.conf.global_settings import MEDIA_ROOT
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 # Quick-start development settings - unsuitable for production
